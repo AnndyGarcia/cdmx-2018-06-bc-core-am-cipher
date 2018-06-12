f39229647709f052676e2cfb024e72f252b29ccb
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-06-12 12:39:38 -0500

    Modificacion del estilo css, adicion de una breve introduccion en html, se respetan espacios al codificar y decodificar en js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 //declaracion de variables y arreglos vacios
 
-let message;
 let string = [];
 let code = [];
-let n;
-let j;
-let newCode = [];
 
 function encodeText() //funcion que codifica el texto
 {
-  message = document.getElementById("fraseInicial").value; //message guarda el valor del texto ingresado por el usuario
-  n = document.getElementById("key").value; //n guarda el numero que se recorre el encriptado
-  for(j = 0; j < message.length; j++){ //for que recorre el message
-  	string.push((message.charCodeAt(j))-65+n%26+65); //se convierte cada letra de message en ASCII y se hace el recorrido con la llave del usuario, asi mismo se guarda el mensaje en ASCII en un arreglo string
-    code.push(String.fromCharCode(string[j])); //se convierte cada elemento ASCII de string en alfanumerico, y se guarda en code
-   }
-  
- newCode = code.join("");
-  
+  let message = document.getElementById("fraseInicial").value; //message guarda el valor del texto ingresado por el usuario
+  let n = parseInt(document.getElementById("key").value); //n guarda el numero que se recorre el encriptado
+  for(let j = 0; j < message.length; j++){ //for que recorre el message
+    if(message.charCodeAt(j)===32){
+      string.push((32)); //detecta los espacios y los mantiene
+      code.push(String.fromCharCode(message.charCodeAt([j])));
+    }
+    else {
+      string.push((message.charCodeAt(j))-65+n%26+65); //se convierte cada letra de message en ASCII y se hace el recorrido con la llave del usuario, asi mismo se guarda el mensaje en ASCII en un arreglo string
+      code.push(String.fromCharCode(string[j])); //se convierte cada elemento ASCII de string en alfanumerico, y se guarda en code
+    }
+  }
+
+ let newCode = code.join("");
+
  document.getElementById("descifrado").innerHTML = (newCode.toUpperCase()); //coloca el mensaje encriptado en un recuadro abajo
-  
+
  /* console.log(string); muestra codigo ASCII
  console.log(code); muestra mensaje encriptado */
  string = []; //vacia la cadena
@@ -28,17 +30,23 @@ function encodeText() //funcion que codifica el texto
 
 function uncodeText() //funcion que decodifica el texto
 {
-  message = document.getElementById("fraseInicial").value; //message guarda el valor del texto ingresado por el usuario
-  n = document.getElementById("key").value; //n guarda el numero que se recorre el desencriptado
-  for(j = 0; j < message.length; j++){ //for que recorre el message
-  	string.push((message.charCodeAt(j))+65-n%26-65); //se convierte cada letra de message en ASCII y se hace el recorrido con la llave del usuario, asi mismo se guarda el mensaje en ASCII en un arreglo string
-  	code.push(String.fromCharCode(string[j])); //se convierte cada elemento ASCII de string en letras, y se guarda en code
+  let message = document.getElementById("fraseInicial").value; //message guarda el valor del texto ingresado por el usuario
+  let n = parseInt(document.getElementById("key").value); //n guarda el numero que se recorre el desencriptado
+  for(let j = 0; j < message.length; j++){ //for que recorre el message
+    if(message.charCodeAt(j)===32){
+      string.push((32)); //detecta los espacios y los mantiene
+      code.push(String.fromCharCode(message.charCodeAt([j])));
+    }
+    else {
+      string.push((message.charCodeAt(j))+65-n%26-65); //se convierte cada letra de message en ASCII y se hace el recorrido con la llave del usuario, asi mismo se guarda el mensaje en ASCII en un arreglo string
+      code.push(String.fromCharCode(string[j])); //se convierte cada elemento ASCII de string en alfanumerico, y se guarda en code
+    }
   }
-  
- newCode = code.join("");
-  
+
+  let newCode = code.join("");
+
  document.getElementById("descifrado").innerHTML = (newCode.toUpperCase()); //coloca el mensaje desencriptado en un recuadro abajo
- 
+
  /* console.log(string); muestra codigo ASCII
  console.log(code); muestra mensaje desencriptado */
  string = []; //vacia la cadena

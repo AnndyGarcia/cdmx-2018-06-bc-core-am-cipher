6bad307b6813475c3fe096f7aca476cb975dd135
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-06-14 13:41:55 -0500

    Se edito el codigo index.js para hacer el enlace a los otros documentos

--- src/index.js
+++ src/index.js
@@ -1,54 +1,20 @@
-//declaracion de variables y arreglos vacios
+let string = document.getElementById("fraseInicial"); //message guarda el valor del texto ingresado por el usuario
+let offset = document.getElementById("key"); //n guarda el numero que se recorre el encriptado
+let botonCifrar = document.getElementById("cipher"); //Boton que acciona la funcion encode
+let botonDecifrar = document.getElementById("decipher"); //Boton que acciona la funcion decode
 
-let string = [];
-let code = [];
+botonCifrar.addEventListener("click", event => { //Se agrega la el evento click sobre el boton cifrar
+  let encodeText = window.cipher.encode( //se llama a la funcion encode
+    offset.value, //se agregan los parametros
+    string.value
+  );
+  document.getElementById("descifrado").innerHTML = (encodeText); //Se sustituye el texto codificado en el cuadro de resultados
+});
 
-function encodeText() //funcion que codifica el texto
-{
-  let message = document.getElementById("fraseInicial").value; //message guarda el valor del texto ingresado por el usuario
-  let n = parseInt(document.getElementById("key").value); //n guarda el numero que se recorre el encriptado
-  for(let j = 0; j < message.length; j++){ //for que recorre el message
-    if(message.charCodeAt(j)===32){
-      string.push((32)); //detecta los espacios y los mantiene
-      code.push(String.fromCharCode(message.charCodeAt([j])));
-    }
-    else {
-      string.push((message.charCodeAt(j))-65+n%26+65); //se convierte cada letra de message en ASCII y se hace el recorrido con la llave del usuario, asi mismo se guarda el mensaje en ASCII en un arreglo string
-      code.push(String.fromCharCode(string[j])); //se convierte cada elemento ASCII de string en alfanumerico, y se guarda en code
-    }
-  }
-
- let newCode = code.join("");
-
- document.getElementById("descifrado").innerHTML = (newCode.toUpperCase()); //coloca el mensaje encriptado en un recuadro abajo
-
- /* console.log(string); muestra codigo ASCII
- console.log(code); muestra mensaje encriptado */
- string = []; //vacia la cadena
- code = []; //vacia la cadena
-}
-
-function uncodeText() //funcion que decodifica el texto
-{
-  let message = document.getElementById("fraseInicial").value; //message guarda el valor del texto ingresado por el usuario
-  let n = parseInt(document.getElementById("key").value); //n guarda el numero que se recorre el desencriptado
-  for(let j = 0; j < message.length; j++){ //for que recorre el message
-    if(message.charCodeAt(j)===32){
-      string.push((32)); //detecta los espacios y los mantiene
-      code.push(String.fromCharCode(message.charCodeAt([j])));
-    }
-    else {
-      string.push((message.charCodeAt(j))+65-n%26-65); //se convierte cada letra de message en ASCII y se hace el recorrido con la llave del usuario, asi mismo se guarda el mensaje en ASCII en un arreglo string
-      code.push(String.fromCharCode(string[j])); //se convierte cada elemento ASCII de string en alfanumerico, y se guarda en code
-    }
-  }
-
-  let newCode = code.join("");
-
- document.getElementById("descifrado").innerHTML = (newCode.toUpperCase()); //coloca el mensaje desencriptado en un recuadro abajo
-
- /* console.log(string); muestra codigo ASCII
- console.log(code); muestra mensaje desencriptado */
- string = []; //vacia la cadena
- code = []; //vacia la cadena
-}
+botonDecifrar.addEventListener("click", event => { //Se agrega la el evento click sobre el boton descifrar
+  let decodeText = window.cipher.decode( //se llama a la funcion decode
+    offset.value, //se agregan los parametros
+    string.value
+  );
+  document.getElementById("descifrado").innerHTML = (decodeText); //Se sustituye el texto codificado en el cuadro de resultados
+});

c53155379ef73bc879c3e6c4c65ca62b41026c58
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-08-03 11:06:54 -0500

    Se agrego guia de estilos de Laboratoria

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-let string = document.getElementById("frase-inicial"); //message guarda el valor del texto ingresado por el usuario
+let string = document.getElementById("initial-phrase"); //message guarda el valor del texto ingresado por el usuario
 let offset = document.getElementById("key"); //n guarda el numero que se recorre el encriptado
-let botonCifrar = document.getElementById("cipher"); //Boton que acciona la funcion encode
-let botonDecifrar = document.getElementById("decipher"); //Boton que acciona la funcion decode
+let cipherButton = document.getElementById("cipher"); //Boton que acciona la funcion encode
+let decipherButton = document.getElementById("decipher"); //Boton que acciona la funcion decode
+let resultAction = document.getElementById("result");
 
-botonCifrar.addEventListener("click", event => { //Se agrega la el evento click sobre el boton cifrar
+cipherButton.addEventListener("click", event => { //Se agrega la el evento click sobre el boton cifrar
   let encodeText = window.cipher.encode( //se llama a la funcion encode
     string.value, //se agregan los parametros
     offset.value
   );
-  document.getElementById("descifrado").innerHTML = (encodeText); //Se sustituye el texto codificado en el cuadro de resultados
+  resultAction.innerHTML = (encodeText); //Se sustituye el texto codificado en el cuadro de resultados
 });
 
-botonDecifrar.addEventListener("click", event => { //Se agrega la el evento click sobre el boton descifrar
+decipherButton.addEventListener("click", event => { //Se agrega la el evento click sobre el boton descifrar
   let decodeText = window.cipher.decode( //se llama a la funcion decode
     string.value, //se agregan los parametros
     offset.value
   );
-  document.getElementById("descifrado").innerHTML = (decodeText); //Se sustituye el texto codificado en el cuadro de resultados
+  resultAction.innerHTML = (decodeText); //Se sustituye el texto codificado en el cuadro de resultados
 });
